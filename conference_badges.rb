9441e883c099cadceda3fa3f753ab7eca177e302
Amanda M <amandaseagull3@gmail.com>
2018-03-27 23:24:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,8 @@
 # Write your code here.
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-# def badge_maker(name)
-#   name = "Arel"
-#   "Hello, my name is #{name}."
-# end
-
 def badge_maker(name)
+  name = "Arel"
   "Hello, my name is #{name}."
 end
 
