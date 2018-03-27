38990ec3ef4aa5c021b4b49d269727e1f9899142
Amanda M <amandaseagull3@gmail.com>
2018-03-27 22:57:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  name = "Arel"
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name="Johnny")
+  badges.each{ |badge| "Hello, my name is #{name}."}
+end
