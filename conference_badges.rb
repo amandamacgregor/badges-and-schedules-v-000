27344bebb55cfb814e7fe05f86c76bb8b90ead7e
Amanda M <amandaseagull3@gmail.com>
2018-03-27 23:09:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,6 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(badge)
+def batch_badge_creator(badges)
   badges.each{ |badge| "Hello, my name is #{badge}."}
 end
