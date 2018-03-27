71695512264e2d4a0b7aef53e27eb35b0fa514e6
Amanda M <amandaseagull3@gmail.com>
2018-03-27 23:43:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 end
 
 def assign_rooms(attendees)
-(attendees).each_with_index.map do |attendee, index|
+(attendees).map.each_with_index do |attendee, index|
   "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
 # hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
